db8f90c0fb75b84d96d3c6ae1e6603651114c09e
abdelrhman1812 <abdelrhmanali1812@gmail.com>
2025-05-29 00:12:40 +0300

    Implement Create Game with validation and middleware for multer and validator

--- src/controllers/game.controller.js
+++ src/controllers/game.controller.js
@@ -11,4 +11,35 @@ const getGames = async (req, res) => {
   }
 };
 
-export { getGames };
+/* ======================= Create all games ======================= */
+
+const createGame = async (req, res) => {
+  try {
+    const { title, description, price, platform, genre } = req.body;
+
+    const coverImage = req.file && req.file.path;
+
+    const newGame = new GameModel({
+      title,
+      description,
+      price,
+      platform,
+      genre,
+      coverImage,
+    });
+    if (coverImage) {
+      newGame.coverImage = coverImage;
+    }
+
+    await newGame.save();
+
+    res
+      .status(201)
+      .json({ message: "Game created successfully", game: newGame });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export { createGame, getGames };
--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
-import { getGames } from "../controllers/game.controller.js";
+import { createGame, getGames } from "../controllers/game.controller.js";
+import validation from "../middlewares/validation.js";
+import uploadFileDisk from "../utils/local.multer.js";
+import { validateCreateGame } from "../validations/game.validation.js";
 
 const gameRouter = Router();
 
 gameRouter.get("/", getGames);
 
+gameRouter.post(
+  "/",
+  uploadFileDisk().single("coverImage"),
+  validateCreateGame,
+  validation,
+  createGame
+);
 export default gameRouter;
